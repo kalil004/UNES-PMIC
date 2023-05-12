c12dac6acd8c741e69125b0c3a32846e5f55a756
Kalil Pereira <kalil.alves.2019@gmail.com>
2023-05-12 08:52:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -18,4 +18,7 @@ def quemsomos():
 def contato():
     return render_template('contatounes.html')
 
+@app.route("/users/")
+def users():
+    return render_template('users.html')
 
